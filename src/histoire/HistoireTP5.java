c57cb19724b1a9fa4398738aa7f47b563d521929
Liam Duneau <dnl5504a@ensfsi.onmicrosoft.com>
2025-04-03 14:30:41 +0200

    Les Samourai (coucou Assassin Screed Origin)

--- src/histoire/HistoireTP5.java
+++ src/histoire/HistoireTP5.java
@@ -2,17 +2,20 @@ package histoire;
 
 import personnages.Commercant;
 import personnages.Ronin;
+import personnages.Samourai;
 import personnages.Yakuza;
 
 public class HistoireTP5 {
 	
 	public static void main(String[] args) {
+		
 		Commercant marco = new Commercant("Marco", 20);
 		Commercant chonin = new Commercant("Chonin", 40);
 		Commercant kumi = new Commercant("Kumi", 10);
 		Yakuza yaku = new Yakuza("Yaku le noir", "whisky", 30, "Warsong");
 		Ronin roro = new Ronin("Roro", "shochu", 60);
 		
+		/*
 		marco.faireConnaissance(roro);
 		marco.faireConnaissance(yaku);
 		marco.faireConnaissance(chonin);
@@ -20,5 +23,11 @@ public class HistoireTP5 {
 		marco.listerConnaissance();
 		roro.listerConnaissance();
 		yaku.listerConnaissance();
+		*/
+		
+		Samourai akimoto = new Samourai("Miyamoto", "Akimoto", "saké", 80);
+		akimoto.faireConnaissance(marco);
+		akimoto.listerConnaissance();
+		akimoto.boire("thé");
 	}
 }
